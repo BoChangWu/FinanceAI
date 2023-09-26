d77d6a0c927f11557e671c9c17b4063d69cb24de
JsonWu <j2223334555@gmail.com>
2023-09-27 01:25:04 +0800

    2023/09/27 01:24 TradingBot

---
+++ Environment.py
@@ -0,0 +1,144 @@
+import os
+import math
+import random
+import numpy as np 
+import pandas as pd
+import yfinance as yf
+import pandas_datareader.data as web
+from pylab import plt,mpl
+os.environ['PYTHONHASHSEED'] = '0'
+plt.style.use('seaborn')
+mpl.rcParams['savefig.dpi'] = 300
+mpl.rcParams['font.family'] = 'serif' 
+
+# 環境觀測空間
+class observation_space:
+    def __init__(self,n):
+        self.shape = (n,)
+
+# 環境的動作空間
+class action_space:
+    def __init__(self,n):
+        self.n = n
+    
+    def sample(self):
+        return random.randint(0,self.n-1)
+    
+
+# 股市環境
+class FinanceStock:
+    intraday = False
+
+    def __init__(self,symbol,features,window,lags,leverage=1,min_performance=0.85,
+                 min_accuracy=0.5,start=0,end=None,mu=None,std=None):
+        self.symbol = symbol
+        self.features = features
+        self.n_features = len(self.features)
+        self.window = window
+        self.lags = lags
+        self.leverage = leverage
+        self.min_performance = min_performance
+        self.min_accuracy = min_accuracy
+        self.start = start
+        self.end = end
+        self.mu = mu
+        self.std = std
+        
+        self._get_data()
+        self._prepare_data()
+
+    def _get_data(self):
+        '''
+        取得指定代號的每日資料(收盤價)(可以擴充更多資訊)
+        '''
+        yf.pdr_override()
+        self.raw = web.get_data_yahoo([self.symbol])
+
+        if self.intraday:
+            self.raw = self.raw.resample('30min',label='right').last()
+    
+    def _prepare_data(self):
+        '''
+        製作更多欄位作為資料
+        '''
+        self.data = pd.DataFrame(self.raw['Close'])
+        self.data.columns = [self.symbol]
+        self.data  = self.data.iloc[int(self.start)*len(self.data)]
+        # 對數報酬率
+        self.data['r'] = np.log(self.data / self.data.shift(1))
+        self.data.dropna(inplace=True)
+        # 策略報酬率
+        self.data['s'] = self.data[self.symbol].rolling(self.window).mean()
+        # 動量
+        self.data['m'] = self.data['r'].rolling(self.window).mean()
+        # 波動率
+        self.data['v'] = self.data['r'].rolling(self.window).std()
+        self.data.dropna(inplace=True)
+
+        if self.mu is None:
+            self.mu = self.data.mean()
+            self.std = self.data.std()
+
+        # 高斯正規化
+        self.data_ = (self.data - self.mu) / self.std
+        # 買賣信號
+        self.data['d'] = np.where(self.data['r'] > 0,1,0)
+        self.data['d'] = self.data['d'].astype(int)
+
+        if self.end is not None:
+            self.data = self.data.iloc[:int(self.end*len(self.data))]
+            self.data_ = self.data_.iloc[:int(self.end*len(self.data))]
+
+    def _get_state(self,bar=None):
+        '''
+        取得狀態
+        '''
+        if bar:
+            self.bar = bar
+
+        return self.data_[self.features].iloc[self.bar-self.lags:self.bar]
+    
+    def seed(self,seed=100):
+        '''
+        設定亂數種子
+        '''
+        random.seed(seed)
+        np.random.seed(seed)
+
+    def reset(self):
+        '''
+        回歸初始設定
+        '''
+        self.treward = 0
+        self.accuracy = 0
+        self.performance = 1
+        self.bar = self.lags
+        state = self.data_[self.features].iloc[self.bar-self.lags:self.bar]
+        
+        return state.values
+
+    def step(self,action):
+        correct = action == self.data['d'].iloc[self.bar]
+        ret = self.data['r'].iloc[self.bar] * self.leverage
+        reward_1 = 1 if correct else 0
+        reward_2 = abs(ret) if correct else -abs(ret)
+        self.treward += reward_1
+        self.bar += 1
+        self.accuracy = self.treward / (self.bar-self.lags)
+        self.performance *= math.exp(reward_2)
+
+        if self.bar >= len(self.data):
+            done = True
+        elif reward_1 == 1:
+            done = False
+        elif (self.performance < self.min_performance) and (self.bar > self.lags + 15):
+            done = True
+        elif (self.accuracy < self.min_accuracy) and (self.bar > self.lags + 15):
+            deon = True
+        else:
+            done = False
+
+        state = self._get_state()
+        info = {}
+
+        return state.values,reward_1+reward_2*5,done,info
---
+++ TradingBot.py
@@ -0,0 +1,182 @@
+import random
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from pylab import plt,mpl
+plt.style.use('seaborn')
+mpl.rcParams['savefig.dpi'] = 300
+mpl.rcParams['font.family'] = 'serif' 
+from collections import deque
+from tensorflow.keras.layers import Dense,Dropout,LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam,RMSprop
+from Environment import FinanceStock
+from sklearn.metrics import accuracy_score
+
+
+
+def set_seeds(seed=100):
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+class TradingBot:
+    def __init__(self,learn_env,valid_env=None,val=True,hidden_units=24,
+                 hidden_layers=1,learning_rate=0.001,dropout=False):
+        self.learn_env = learn_env
+        self.valid_env = valid_env
+        self.val = val
+        self.epsilon = 1.0
+        self.epsilon_min = 0.1
+        self.epsilon_decay = 0.99
+        self.learning_rate = learning_rate
+        self.gamma = 0.5
+        self.batch_size = 128
+        self.max_treward = 0
+        self.averages = list()
+        self.trewards = list()
+        self.performances = list()
+        self.aperformances = list()
+        self.vperformances = list()
+        self.memory = deque(maxlen=200)
+        self.train_histories = list()
+        self.model = self._build_model(hidden_units,hidden_layers,learning_rate,dropout)
+
+
+    def _build_model(self,hu,hl,lr,dropout):
+        '''
+        根據參數建立模型(DNN)
+        '''
+        model = Sequential()
+        model.add(Dense(hu,input_shape=(self.learn_env.lags,self.learn_env.n_features),activation='relu'))
+        if dropout:
+            model.add(Dropout(0.3,seed=100))
+        
+        for _ in range(hl):
+            model.add(Dense(hu,activation='relu'))
+            if dropout:
+                model.add(Dropout(0.3,seed=100))
+
+        model.add(Dense(2,activation='linear'))
+        model.compile(loss='mse',optimizer=RMSprop)
+        return model
+    
+    def act(self,state):
+        '''
+        依 a) 探索, b) 利用 而採取行動
+        '''
+        if random.random() <= self.epsilon:
+            return self.learn_env.action_space.sample()
+        
+        action = self.model.predict(state)[0,0]
+        return np.argmax(action)
+    
+    def replay(self):
+        '''
+        以批量的記憶經驗重新訓練DNN模型的方法
+        '''
+        batch = random.sample(self.memory,self.batch_size)
+
+        for state,action,reward,next_state,done in batch:
+            if not done:
+                reward += self.gamma * np.amax(self.model.predict(next_state)[0,0])
+            target = self.model.predict(state)
+            target[0,0,action] = reward
+            h = self.model.fit(state,target,epochs=1,verbose=False)
+            self.train_histories.append(h)
+        
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def learn(self,episodes):
+        '''
+        訓練DQL代理人
+        '''
+        for e in range(1,episodes+1):
+            state = self.learn_env.reset()
+            state = np.reshape(state,[1,self.learn_env.lags,self.learn_env.n_features])
+
+            for _ in range(10000):
+                action = self.act(state)
+                next_state,reward,done,info = self.learn_env.step(action)
+                next_state = np.reshape(next_state,[1,self.learn_env.lags,self.learn_env.n_features])
+                self.memory.append([state,action,reward,next_state,done])
+                
+                state = next_state
+
+                if done:
+                    treward = _ + 1
+                    self.trewards.append(treward)
+                    av = sum(self.trewards[-25:]) / 25
+                    perf = self.learn_env.performance
+                    self.averages.append(av)
+                    self.performances.append(perf)
+                    self.aperformances.append(sum(self.performances[-25:]) / 25)
+                    self.max_treward = max(self.max_treward,treward)
+
+                    templ = 'episode: {:2d}/{} | treward: {:4d} | perf: {:5.3f} | av: {:5.1f} | max: {:4d}'
+                    print(templ.format(e,episodes,treward,perf,av,self.max_treward))
+                    break
+        
+        if self.val:
+            self.validate(e,episodes)
+        if len(self.memory) > self.batch_size:
+            self.replay()
+
+    def validate(self,e,episodes):
+        '''
+        驗證DQL代理人
+        '''
+        state = self.valid_env.reset()
+        state = np.reshape(state,[1,self.valid_env.lags,self.valid_env.n_features])
+
+        for _ in range(10000):
+            action = np.argmax(self.model.predict(state)[0,0])
+            next_state,reward,done,info = self.valid_env.step(action)
+            state = np.reshape(next_state,[1,self.valid_env.lags,self.valid_env.n_features])
+
+            if done:
+                treward = _ + 1
+                perf = self.valid_env.performance
+                self.vperformances.append(perf)
+
+                if e % int(episodes / 6) == 0:
+                    templ = 71*'='
+                    templ = '\n episode: {:2d}/{} | VALIDATION | treward: {:4d} | perf: {:5.3f} | eps: {:.2f}\n'
+                    templ += 71*'='
+                    print(templ.format(e,episodes,treward,perf,self.epsilon))
+
+                break
+
+def plot_treward(agent):
+    '''
+    視覺化每個訓練episode總獎勵
+    '''
+    plt.figure(figsize=(10,6))
+    x = range(1,len(agent.averages)+1)
+    y = np.polyval(np.polyfit(x,agent.averages,deg=3),x)
+    plt.plot(x,agent.averages,label='moving average')
+    plt.plot(x,y,'r--',labl='regression')
+    plt.xlabel('episodes')
+    plt.ylabel('total reward')
+    plt.legend()
+
+def plot_performance(agent):
+    '''
+    視覺化每個episode金融毛績效
+    '''
+    plt.figure(figsize=(10,6))
+    x = range(1,len(agent.performances)+1)
+    y = np.polyval(np.polyfit(x,agent.performances,deg=3),x)
+    plt.plot(x,agent.performances[:],label='training')
+    plt.plot(x,y,'r--',label='regression (train)')
+
+    if agent.val:
+        y_ = np.polyval(np.polyfit(x,agent.vperformances,deg=3),x)
+        plt.plot(x,agent.vperfomances[:],label='validation')
+        plt.plot(x,y_,'r--',label='regression (valid)')
+
+    plt.xlabel('episodes')
+    plt.ylabel('gross performance')
+    plt.legend()
+    
